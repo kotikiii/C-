ba069ee25f515eb4cfff54e06a1eecfda398873d
robiya <robiya365@icloud.com>
2023-03-04 00:10:55 +0500

    cw task1

---
+++ seminar1/classwork_task_1/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("Pull a nember");
+string userAnswer = Console.ReadLine();
+int number = Convert.ToInt32(userAnswer);
+
+int result = number * number;
+Console.WriteLine(result);
