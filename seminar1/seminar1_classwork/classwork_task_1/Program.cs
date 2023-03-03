cfe14c32743a56f41af4ccc3f37733392769ebb8
robiya <robiya365@icloud.com>
2023-03-04 00:53:42 +0500

    task2

--- seminar1/seminar1_classwork/classwork_task_1/Program.cs
+++ seminar1/seminar1_classwork/classwork_task_1/Program.cs
@@ -1,7 +1,7 @@
 ﻿
-Console.WriteLine("Pull a nember");
+Console.Write("Pull a nember");
 string userAnswer = Console.ReadLine();
 int number = Convert.ToInt32(userAnswer);
 
 int result = number * number;
-Console.WriteLine(result);
+Console.Write(result);
---
+++ seminar1/siminar1_homework/hw1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Pull the first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Pull the second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > b)
+{
+    Console.WriteLine("max = " + max);
+}
+
+else
+{
+    max = b;
+    Console.WriteLine("max = " + max);
+}
