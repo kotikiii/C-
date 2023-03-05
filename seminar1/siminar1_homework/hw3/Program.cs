f1d2e3720a1b368a49ddcd0e08a8f3243b312a8f
robiya <robiya365@icloud.com>
2023-03-05 20:10:58 +0500

    task4

--- seminar1/siminar1_homework/hw3/Program.cs
+++ seminar1/siminar1_homework/hw3/Program.cs
@@ -1,6 +1,6 @@
-﻿
-Console.WriteLine("Pull the number");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
+Console.WriteLine("Pull the number");
 int a = Convert.ToInt32(Console.ReadLine());
 int result = a % 2;
 
---
+++ seminar1/siminar1_homework/hw4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.WriteLine("Pull a number ");
+int N = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+
+while (current <= N)
+{
+    if (current % 2 == 0)
+    {
+        Console.Write(current + " ");
+    }
+    current = current + 1;
+}
+
