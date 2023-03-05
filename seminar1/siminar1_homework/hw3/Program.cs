76e2236a2b59202c401ba57d8d9ab83cdf032566
robiya <robiya365@icloud.com>
2023-03-05 18:45:48 +0500

    task3

---
+++ seminar1/siminar1_homework/hw3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Pull the number");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int result = a % 2;
+
+if (result == 0)
+{
+ Console.WriteLine("The number is even");
+}
+
+else{
+Console.WriteLine("The number is not even");
+}
