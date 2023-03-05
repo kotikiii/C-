06354440758bee5e8c44d522a7c3def967bab4cf
robiya <robiya365@icloud.com>
2023-03-05 18:14:07 +0500

    final

--- seminar1/seminar1_classwork/classwork_task_1/Program.cs
+++ seminar1/seminar1_classwork/classwork_task_1/Program.cs
@@ -1,7 +1,7 @@
 ﻿
-Console.Write("Pull a nember");
-string userAnswer = Console.ReadLine();
-int number = Convert.ToInt32(userAnswer);
+// Console.Write("Pull a nember");
+// string userAnswer = Console.ReadLine();
+// int number = Convert.ToInt32(userAnswer);
 
-int result = number * number;
-Console.Write(result);
+// int result = number * number;
+// Console.Write(result);
