233ed74aaf568b4cbe84b403d7bd2781b3a5b3e5
robiya <robiya365@icloud.com>
2023-03-05 23:09:15 +0500

    task1 completed

---
+++ seminar2/seminar2_cw/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿                            // (minValue, maxValue]
+int randomNumber = Random.Shared.Next(10, 100); 
+Console.WriteLine(randomNumber);
+
+int firstDigit = randomNumber / 10;
+Console.WriteLine("The first digit is " + firstDigit);
+
+int lastDigit = randomNumber % 10;
+Console.WriteLine("The last digit is " + lastDigit);
+
+if (lastDigit>firstDigit)
+{
+    Console.WriteLine("The max is " + lastDigit);
+}
+
+else {
+        Console.WriteLine("The max is " + firstDigit);
+}
