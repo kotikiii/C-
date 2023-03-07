401ccc32dae70905bfd8655a676a97955c1bfbbc
robiya <robiya365@icloud.com>
2023-03-07 23:51:13 +0500

    task3 completed

---
+++ seminar2/seminar2_cw/task3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.WriteLine("Pull the number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 7 == 0 && a % 23 == 0)
+{
+  Console.WriteLine("Divisable by 7 and 23");
+}
+
+else {
+    Console.WriteLine("Non-Divisable by 7 or/and 23");
+}
