846cb3316a34486902c2845d47515b9e3bb1ea6a
robiya <robiya365@icloud.com>
2023-03-08 16:33:58 +0500

    sqr root and power(daraja)

---
+++ seminar3/seminar3_cw/mathTricks/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+int a = 3;
+int b = 4;
+
+double result = Math.Pow(a, b);
+result = Math.Sqrt(result);
+Console.WriteLine(result);
+
+// Math.Pow( a, b) darajaga oshirish; 'a' bu son, 'b' nu daraja korsatkichi. 
+// Math.Sprt() square root.
