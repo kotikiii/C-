4df0c316cc3ef889e2d0f83e8ac925b3c72d85aa
robiya <robiya365@icloud.com>
2023-03-08 12:22:31 +0500

    task2 completed

---
+++ seminar2/seminar2_hw/task2/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Pull a number: ");
+int usersNumber = Convert.ToInt32(Console.ReadLine());
+
+if (usersNumber < 100)
+{
+    Console.WriteLine("Third digit does not exist");
+}
+
+if (usersNumber >= 100 && usersNumber < 1000)
+{
+    int thirdDigit = usersNumber % 10;
+    Console.WriteLine(thirdDigit);
+}
+
+if (usersNumber >= 1000 && usersNumber < 10000)
+{
+    int lastTwoDigits = usersNumber % 100;
+    int thirdDigit = lastTwoDigits / 10;
+    Console.WriteLine(thirdDigit);
+}
+
+if (usersNumber >= 10000 && usersNumber < 100000)
+{
+    int lastThreeDigits = usersNumber % 1000;
+    int thirdDigit = lastThreeDigits / 100;
+    Console.WriteLine(thirdDigit);
+}
