93cae5bbf1735162d1749623671fba7b9e37f104
robiya <robiya365@icloud.com>
2023-03-08 11:55:46 +0500

    task1 completed

---
+++ seminar2/seminar2_hw/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Pull a number: ");
+int ThreeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+int lastTwoDigits = ThreeDigitNumber % 100;
+int middleDigit = lastTwoDigits / 10;
+
+Console.WriteLine(middleDigit);
