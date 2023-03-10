ccab989b0f1ae7b91c419216fc720262c39397ca
robiya <robiya365@icloud.com>
2023-03-11 00:14:21 +0500

    hw-task1-sem3

---
+++ seminar3/seminar3_hw/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Pull a number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == (number % 10) * 10000 + ((number % 100) / 10) * 1000 + number % 1000)
+{
+    Console.WriteLine("yes " + number);
+}
+else
+{
+    Console.WriteLine("no");
+}
