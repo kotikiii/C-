61959bc5a85012365483eebf8f092dafe744a4ca
robiya <robiya365@icloud.com>
2023-03-10 23:42:32 +0500

    task3 completed

---
+++ seminar3/seminar3_cw/task3/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.WriteLine("Pull a number ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int numbers = 1;
+
+while ( numbers <= N)
+{
+   double sqearedNumbers = Math.Pow( numbers, 2);
+   Console.Write(" " + sqearedNumbers);
+   numbers = numbers + 1;
+}
+
