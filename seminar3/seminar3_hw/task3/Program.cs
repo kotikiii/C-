54363bf925a9b3d8846c63e41994d5f8e3122fc8
robiya <robiya365@icloud.com>
2023-03-11 00:39:50 +0500

    hw-task3-sem3

---
+++ seminar3/seminar3_hw/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Pull a nember: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int numbers = 1;
+while (numbers <= N)
+{
+    double cubed = Math.Pow( numbers , 3);
+    Console.Write(" " + cubed);
+    numbers++;
+}
