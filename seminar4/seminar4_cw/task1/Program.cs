b6769c840ede2652946b3be0a3c836d4ee161c94
robiya <robiya365@icloud.com>
2023-03-13 22:10:11 +0500

    task1 cw sem4

---
+++ seminar4/seminar4_cw/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int ReadIntNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int sumFrom1ToA(int a)
+{
+    int sum = 0;
+
+    for (int i = 1; i <= a; i++)
+    {
+       sum = sum + i;
+       // sum += i;
+    }
+    
+    return sum;
+}
+
+int number = ReadIntNumber("Pull a number: ");
+int result = sumFrom1ToA(number);
+
+Console.WriteLine($"Sum of the numbers from 1 to {number} is {result}");
