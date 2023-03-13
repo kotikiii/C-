5127ec8b534f14f5ba8b76fac2a5daef956a49eb
robiya <robiya365@icloud.com>
2023-03-13 22:54:24 +0500

    task2 cw sem4

---
+++ seminar4/seminar4_cw/task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+Console.WriteLine("Pull a number: ");
+
+int numberOfDigit(int n)
+{
+    int digits = 1;
+    while (n > 10)
+    {
+        n /= 10;
+        digits++;
+    }
+    return digits;
+}
+
+int number = Convert.ToInt32(Console.ReadLine());
+int res = numberOfDigit(number);
+Console.WriteLine(res);
