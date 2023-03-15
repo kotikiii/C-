f0a8c8ab117a2a622e20c36c20fc220cac87b267
robiya <robiya365@icloud.com>
2023-03-15 21:30:56 +0500

    couldnt solve task2 hw sem4

---
+++ seminar4/seminar4_hw/task2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int userNumber(string messageToUser)
+{
+    Console.Write(messageToUser);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int summOfDigits(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10
+    }
+    return result;
+}
+int number = userNumber("Pull your number: ");
+int res = summOfDigits(number);
+System.Console.WriteLine(res);
