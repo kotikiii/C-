b394d86e96077db567d9308b50d80c7267aabcc2
robiya <robiya365@icloud.com>
2023-03-15 22:00:03 +0500

    couldnt complete task3 hw sem4

---
+++ seminar4/seminar4_hw/task3/Program.cs
@@ -0,0 +1,29 @@
+﻿
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
+
+int[] array = newArray[8];
+Console.WriteLine(printArray(array));
+
+int interval(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int intervalA = interval("Pull maxsimum possible value");
+int intervalB = interval("Pull minimal possible value");
+
+int newArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        int num = Random.Shared.Next(intervalB, intervalA + 1);
+        array[i] = num;
+    }
+    return array;
+}
+
+void printArray (int [] array){
+Console.WriteLine(string.Join(", ", newArray));
+}
