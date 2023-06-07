a18ed09957fb6fae88a263e3217c6286566736a0
robiya <robiya365@icloud.com>
2023-06-07 23:36:31 +0500

    sem3 completed

--- seminar3/seminar3_hw/task1/Program.cs
+++ seminar3/seminar3_hw/task1/Program.cs
@@ -5,14 +5,42 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.WriteLine("Pull a number");
+// Console.WriteLine("Pull a number");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number == (number % 10) * 10000 + ((number % 100) / 10) * 1000 + number % 1000)
+// {
+//     Console.WriteLine("yes " + number);
+// }
+// else
+// {
+//     Console.WriteLine("no");
+// }
+
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number == (number % 10) * 10000 + ((number % 100) / 10) * 1000 + number % 1000)
+bool Check ( int n )
 {
-    Console.WriteLine("yes " + number);
+    string newN = Convert.ToString(n);
+    int a = 0;
+    int b = 0;
+    for (int i = 0; i <2; i++)
+    {
+        for (int j = 4; j > 2; j=j-1)
+        {
+            a = newN[i];
+            b = newN[j];
+        }
+    };
+    return a==b;
 }
-else
-{
+
+bool isItPalindrom = Check(number);
+
+if (isItPalindrom) {
+    Console.WriteLine("yes");
+}
+else {
     Console.WriteLine("no");
 }
+
--- seminar3/seminar3_hw/task3/Program.cs
+++ seminar3/seminar3_hw/task3/Program.cs
@@ -4,13 +4,24 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
-Console.Write("Pull a nember: ");
-int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Pull a number: ");
+int N = int.Parse(Console.ReadLine());
 
-int numbers = 1;
-while (numbers <= N)
+void PrintCubed(int n)
 {
-    double cubed = Math.Pow( numbers , 3);
-    Console.Write(" " + cubed);
-    numbers++;
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write( Math.Pow(i, 3));
+        Console.Write(" ");
+    }
 }
+
+PrintCubed(N);
+
+// int numbers = 1;
+// while (numbers <= N)
+// {
+//     double cubed = Math.Pow( numbers , 3);
+//     Console.Write(" " + cubed);
+//     numbers++;
+// }
